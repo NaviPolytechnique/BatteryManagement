b2da2772e96bb144e218f62c229656a395f6e27f
Gogo56 <Hugz@MacBook-Pro-de-Utilisateur.local>
2015-09-07 16:00:47 +0200

    Débuggage

--- Batt_management.cpp
+++ Batt_management.cpp
@@ -7,88 +7,89 @@
 //
 
 #include "Batt_management.h"
-#include <AP_HAL.h>
-#include <AP_Common.h>
-#include <AP_Math.h>
 
-Batt_management(){
+Batt_management::Batt_management(){
     
 }
 
-Batt_management::Batt_management(int pinV, int pinC, float capacity){
-    pin_volt = pinV;
-    pin_current= pinC;
+Batt_management::Batt_management(float capacity){
+    
     batt_maxcapacity=capacity;
+    min_capacity=0;
+    Adafruit_INA219 ina219;
+    charge_mAh=capacity;
+    time_check = 0;
+    time_low_volt=0;
+    low_volt_duration=0;
+    current_A=0;
     
 }
 
-void Batt_management::setup{
-
-// Setup the INA219.
-ina219.begin();
-// By default the INA219 will be calibrated with a range of 32V, 2A.
-// However uncomment one of the below to change the range.  A smaller
-// range can't measure as large of values but will measure with slightly
-// better precision.
-//ina219.setCalibration_32V_1A();
-//ina219.setCalibration_16V_400mA();
+void Batt_management::setup(){
+    
+    // Setup the INA219.
+    ina219.begin();
+    // By default the INA219 will be calibrated with a range of 32V, 2A.
+    // However uncomment one of the below to change the range.  A smaller
+    // range can't measure as large of values but will measure with slightly
+    // better precision.
+    //ina219.setCalibration_32V_1A();
+    //ina219.setCalibration_16V_400mA();
 }
 
 void Batt_management::read(){
-    uint32_t now_time = hal.scheduler->micros();
+    uint32_t now_time = micros();
     
     // Read voltage and current from INA219.
     float shuntvoltage = ina219.getShuntVoltage_mV();
     float busvoltage = ina219.getBusVoltage_V();
-    current_A = ina219.getCurrent_mA() * 1000;
+    current_A = ina219.getCurrent_mA();
     
     // Compute load voltage, power, and milliamp-hours.
-    voltage_V = busvoltage + (shuntvoltage / 1000);
-    float power_mW = loadvoltage * current_mA;
-    charge_mAh -= current_mA*(time_check-now_time)/3600000000;
+    voltage_V = busvoltage + (shuntvoltage / 1000.0);
+    float power_mW = voltage_V * (current_A*1000.0);
+    charge_mAh -= current_A*(time_check-now_time)/3600000000.0;
     
-    time_check = hal.scheduler->micros(); //reser the new time of reading
+    time_check = micros(); //reset the new time of reading
 }
 
 float Batt_management::voltage() const {
-    return const Batt_state.voltage_V;
+    return  voltage_V;
 }
 
 float Batt_management::current() const {
-    return const Batt_state.currant_A;
+    return  current_A;
 }
 
 float Batt_management::capacity_remaining() const {
-    return Batt_state.charge_mAh;
+    return charge_mAh;
 }
 
 float Batt_management::capacity_remaining_pct() const {
-    return Batt_state.charge_mAh/batt_maxcapacity;
+    return charge_mAh/batt_maxcapacity * 100.0 ;
 }
 
 bool Batt_management::is_exhausted(float low_voltage,float min_capacity){
     
     
-    uint32_t now_time = hal.scheduler->micros();
-    if ((Batt_state.voltage_V > 0) && (low_voltage>0) && Batt_state.voltage_V<low_voltage) // check if there is some voltage
+    uint32_t now_time = micros();
+    if ((voltage_V > 0) && (low_voltage>0) && voltage_V<low_voltage) // check if there is some voltage
     {
         
-        if(Batt_state.time_low_volt == 0){ // first time below low_voltage so we set the timer
-            Batt_state.time_low_volt = now_time;
+        if(time_low_volt == 0){ // first time below low_voltage so we set the timer
+            time_low_volt = now_time;
         }
-        else if ((now_time - Batt_state.time_low_volt) > Batt_state.low_volt_duration)
+        else if ((now_time - time_low_volt) > low_volt_duration)
             return true;
     }else { //not exhausted so we reset the timer
-        Batt_state.time_low_volt = 0;
+        time_low_volt = 0;
     }
-
-    if (Batt_state.current_A > 0 && Batt_state.charge_mAh < min_capacity){
+    
+    if (current_A > 0 && charge_mAh < min_capacity){
         return true;
     }
     else {
         return false; // The battery is ok
     }
 }
-
-
 
--- Batt_management.h
+++ Batt_management.h
@@ -10,16 +10,12 @@
 #define __Battery__Batt_management__
 
 #include <stdio.h>
-#include <chrono>
 #include <Wire.h>
-#include <AP_HAL.h>
-#include <AP_Common.h>
-#include <AP_Math.h>
-#include <Adafruit_INA219.h>
+#include "Adafruit_INA219.h"
 
 class Batt_management {
     
-protected :
+    protected :
     
     float batt_maxcapacity; //the maximum capacity
     float min_capacity;     //minimum capacity accepted
@@ -33,34 +29,34 @@ public:
     
     Batt_management();
     
-    Batt_management(int pinV, int pinC, float capacity);
+    Batt_management(float capacity);
     
     void setup();
     
-    struct Batt_state{ //Battery state
-        
-        float voltage_V;    //voltage in Volt
-        float current_A;    //current in Ampere
-        float charge_mAh;   //current charge of the battery in milli Ampere hours
-        uint32_t time_check;       //last time the battery was checked
-        uint32_t time_low_volt;    //time of low voltage
-        
-        uint32_t low_volt_duration;
-        
-    };
+    //struct Batt_state{ //Battery state
+    
+    float voltage_V;    //voltage in Volt
+    float current_A;    //current in Ampere
+    float charge_mAh;   //current charge of the battery in milli Ampere hours
+    uint32_t time_check;       //last time the battery was checked
+    uint32_t time_low_volt;    //time of low voltage
+    
+    uint32_t low_volt_duration;
+    
+    //  };
     
     // read voltage and current and compute charge
     void read();
     
     // return current voltage
-    float voltage();
+    float  voltage()const;
     
     
-    float current();
+    float current() const;
     
-    float capacity_remaining();
+    float capacity_remaining() const;
     
-    float capacity_remaining_pct();
+    float capacity_remaining_pct() const;
     
     bool is_exhausted(float low_voltage,float min_capacity);
 };
